4c99f0f18b62c9f9ebe595dc70f868faee01c62b
hanith <hanithhoney143@gmail.com>
2020-10-16 20:39:40 +0530

    updated -hanith
    Signed-off-by: hanith <hanithhoney143@gmail.com>

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,83 +1,139 @@
-var express 	= require("express");
-var router  	= express.Router({mergeParams:true});
-var middleware	= require("../middleware");
-var Cart		= require("../models/cart");
-var Order       = require("../models/checkout");
+var express = require("express");
+var router = express.Router({ mergeParams: true });
+var middleware = require("../middleware");
+var Cart = require("../models/cart");
+var Order = require("../models/checkout");
+const stripe = require('stripe')('sk_test_51H7gTXJPyl22FwkuMII2d29sIG6j8f8WOtlFCrtieze4ULa3VvMva255H7nZc9JwaT48gZREhfJte6F0ZPibQYTp004cq9HoXW');
+var fast2sms = require("fast-two-sms");
+var api = 'C24tnUhjNGYiWxkQmHpSRFgfluJszZ83OoDATv0VPwLI7Mb91KqK0ubBCLZOhr9Ps8XQWfgIedDEiGt1';
+
+
+
 
 //INDEX ROUTE
-router.get("/", function(req,res,next){
-	if(!req.session.cart){
-		return res.render("cart/pay",{products:null});
-	}
-	var cart = new Cart(req.session.cart);
-	var arr = Object.values(cart.items);
-	res.render("cart/pay",{items:arr,totalPrice:cart.totalPrice});
+router.get("/", function (req, res, next) {
+    if (!req.session.cart) {
+        return res.render("cart/pay", { products: null });
+    }
+    var cart = new Cart(req.session.cart);
+    var arr = Object.values(cart.items);
+    res.render("cart/pay", { items: arr, totalPrice: cart.totalPrice });
 });
 
-router.post("/",function(req,res){
+router.post("/", function (req, res) {
     var cart = new Cart(req.session.cart);
     var arr = Object.values(cart.items);
     // console.log(arr)
-    let items=[]
-    for(i of arr){
+    let items = []
+    for (i of arr) {
         let p = {
             name: i.item.name,
             price: i.price,
-            quantity:i.quantity
+            quantity: i.quantity
         }
         items.push(p)
     }
     // console.log(items)
     // get data from form to post
-    Date.prototype.toShortFormat = function() {
+    Date.prototype.toShortFormat = function () {
 
-        let monthNames =["Jan","Feb","Mar","Apr",
-                          "May","Jun","Jul","Aug",
-                          "Sep", "Oct","Nov","Dec"];
-        
+        let monthNames = ["Jan", "Feb", "Mar", "Apr",
+            "May", "Jun", "Jul", "Aug",
+            "Sep", "Oct", "Nov", "Dec"];
+
         let day = this.getDate();
-        
+
         let monthIndex = this.getMonth();
         let monthName = monthNames[monthIndex];
-        
+
         let year = this.getFullYear();
-        
-        return `${day}-${monthName}-${year}`;  
+
+        return `${day}-${monthName}-${year}`;
     }
-    
-    var date= new Date().toShortFormat()
+
+    var date = new Date().toShortFormat()
     console.log(date)
-    
-	var newOrder={
-        blockToDeliver:req.body.blockToDeliver,
-        cardName:req.body.cardName,
-        cardNumber:req.body.cardNumber,
-        cvv:req.body.cvv,
-        expireMonth:req.body.expireMonth,
-        expireYear:req.body.expireYear,
-        items:items,
-        status:"ordered",
-        createdAt:date,
-        author : {
-            id:req.user._id,
-            username:req.user.username
-        },
-        price:cart.totalPrice
 
-    };
 
-    Order.create(newOrder, function(err){
-		if(err)
-			console.log(err);
-		else{
-            req.session.cart=null
-            // redirect to track page
-			res.redirect("/items");
-        }
-			
-	});
-	
+    try {
+        stripe.customers
+            .create({
+                name: req.body.name,
+                email: req.body.email,
+                source: req.body.stripeToken
+            })
+            .then(customer =>
+                stripe.charges.create({
+                    amount: req.body.amount * 100,
+                    currency: "INR",
+                    customer: customer.id,
+                    description: 'food order',
+                    shipping: {
+                        name: req.body.name,
+                        address: {
+                            line1: req.body.blockToDeliver,
+                            postal_code: '500059',
+                            city: 'HYDERABAD',
+                            state: 'TS',
+                            country: 'IN',
+                        },
+                    },
+                })
+            )
+            .then(() => {
+
+                var options = { authorization: api, message: 'Holla amigo! Order placed. It will reach you by time. check your status here brewhub.com', numbers: [req.user.mobile] };
+                fast2sms.sendMessage(options)
+                console.log(options)
+                var newOrder = {
+                    blockToDeliver: req.body.blockToDeliver,
+
+                    items: items,
+                    status: "ordered",
+                    createdAt: date,
+                    author: {
+                        id: req.user._id,
+                        username: req.user.username
+                    },
+                    price: cart.totalPrice
+
+                };
+
+                Order.create(newOrder, function (err) {
+                    if (err)
+                        console.log(err);
+                    else {
+
+                        req.session.cart = null
+                        // redirect to track page
+                        res.redirect("/items");
+                    }
+
+                });
+
+
+
+            })
+            .catch(err => console.log(err));
+    } catch (err) {
+        res.send(err);
+    }
+
+
+
+
+
+
+
 });
-
+// cardName: req.body.cardName,
+// cardNumber: req.body.cardNumber,
+// cvv: req.body.cvv,
+// expireMonth: req.body.expireMonth,
+// expireYear: req.body.expireYear,
 
 module.exports = router;
+
+
+
+
--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,14 @@
-var express			= require("express"),
-	router			= express.Router(),
-	Order			= require("../models/checkout"),
-	middleware 		= require("../middleware");
+var express = require("express"),
+	router = express.Router(),
+	Order = require("../models/checkout"),
+	middleware = require("../middleware");
+var fast2sms = require("fast-two-sms");
+
+var api = 'C24tnUhjNGYiWxkQmHpSRFgfluJszZ83OoDATv0VPwLI7Mb91KqK0ubBCLZOhr9Ps8XQWfgIedDEiGt1';
+
 
 router.get("/", function (req, res, next) {
-	if(req.user.isAdmin){
+	if (req.user.isAdmin) {
 		Order.find({}, function (err, allOrders) {
 			if (err)
 				console.log(err);
@@ -12,34 +16,37 @@ router.get("/", function (req, res, next) {
 				res.render("orders/index", { orders: allOrders, currentUser: req.user });
 		});
 	}
-	else{
-		Order.find({'author.id':req.user._id}, function (err, allOrders) {
+	else {
+		Order.find({ 'author.id': req.user._id }, function (err, allOrders) {
 			if (err)
 				console.log(err);
 			else
 				res.render("orders/index", { orders: allOrders, currentUser: req.user });
 		});
 	}
-	
+
 });
 
-router.get("/:id",async function(req,res){
-	try{
-		let order =await Order.findById(req.params.id)
+router.get("/:id", async function (req, res) {
+	try {
+		let order = await Order.findById(req.params.id)
 		// console.log(order);
-    	if(req.user._id.toString() === order.author.id.toString()){
-        	res.render("cart/trackPage",{order:order});
-    	}
-    	res.redirect("/items");
+		if (req.user._id.toString() === order.author.id.toString()) {
+			res.render("cart/trackPage", { order: order });
+		}
+		res.redirect("/items");
 	}
-	catch(e){
-		
+	catch (e) {
+
 	}
-	
+
 });
 
 router.get("/:id/dispatched", function (req, res) {
 	Order.where({ _id: req.params.id }).updateOne({ status: "dispatched" }).exec();
+	var options = { authorization: api, message: 'Holla amigo! Order dispatched to your destination. It will reach you by time. check your status here brewhub.com', numbers: [req.user.mobile] };
+	fast2sms.sendMessage(options)
+	console.log(options)
 	req.flash("success", "Order status updated!");
 	res.redirect('back');
 
@@ -47,15 +54,29 @@ router.get("/:id/dispatched", function (req, res) {
 
 router.get("/:id/reached", function (req, res) {
 	Order.where({ _id: req.params.id }).updateOne({ status: "reached" }).exec();
+	var options = { authorization: api, message: 'Holla amigo! Reached: Order reached to your destination. It will reach you by time. check your status here brewhub.com', numbers: [req.user.mobile] };
+	fast2sms.sendMessage(options)
+	console.log(options)
 	req.flash("success", "Order status updated!");
 	res.redirect('back');
 });
 
 router.get("/:id/delivered", function (req, res) {
 	Order.where({ _id: req.params.id }).updateOne({ status: "delivered" }).exec();
+	var options = { authorization: api, message: 'Holla amigo! Delivered: Order has been delivered. Enjoy the food and gives us feedback.', numbers: [req.user.mobile] };
+	fast2sms.sendMessage(options)
+	console.log(options)
 	req.flash("success", "Order status updated!");
 	res.redirect('back');
 });
+
+router.get("/:id/cancel", function (req, res) {
+	Order.where({ _id: req.params.id }).updateOne({ status: "cancelled" }).exec();
+	var options = { authorization: api, message: 'Holla amigo! cancelled: Your order has beed cancelled. Feel free to gives us feedback.', numbers: [req.user.mobile] };
+	fast2sms.sendMessage(options)
+	req.flash("success", "order has been cancelled");
+	res.redirect('back');
+})
 
 
 
